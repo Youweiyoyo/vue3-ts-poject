0bf648c97639dd93a3c34d5fae8b9c01e8f0562c
Youwei <yoyoyo9527@163.com>
2021-10-13 23:00:50 +0800

    用户中心与面包屑导航页面搭建完成 by Youwei

---
+++ src/components/nav-header/components/user-info.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="user-info">
+    <el-dropdown>
+      <span class="el-dropdown-link">
+        <el-avatar shape="circle" size="small" :src="url"></el-avatar>
+        <span>{{ name }}</span>
+      </span>
+      <template #dropdown>
+        <el-dropdown-menu>
+          <el-dropdown-item>登出</el-dropdown-item>
+          <el-dropdown-item>用户信息</el-dropdown-item>
+          <el-dropdown-item>系统管理</el-dropdown-item>
+        </el-dropdown-menu>
+      </template>
+    </el-dropdown>
+  </div>
+</template>
+<script>
+import { defineComponent, computed } from 'vue';
+import { useStore } from '@/store';
+export default defineComponent({
+  setup() {
+    const store = useStore();
+    const name = computed(() => store.state.login.userInfo.name);
+    return {
+      name
+    };
+  }
+});
+</script>
+<style lang="less" scoped>
+.el-dropdown-link {
+  display: flex;
+  align-items: center;
+  vertical-align: middle;
+  cursor: pointer;
+  .el-avatar {
+    margin-right: 10px;
+  }
+}
+</style>
--- src/components/nav-header/src/nav-header.vue
+++ src/components/nav-header/src/nav-header.vue
@@ -1,16 +1,24 @@
 <template>
-  <div class="fold-menu">
+  <div class="fold-menu-nav">
     <i
       class="fold-menu"
       :class="isFold ? ' el-icon-s-fold' : 'el-icon-s-unfold'"
       @click="handleFoldClick"
     ></i>
+    <div class="content">
+      <div>面包屑</div>
+      <user-info />
+    </div>
   </div>
 </template>
 
 <script>
 import { defineComponent, ref } from 'vue';
+import UserInfo from '../components/user-info.vue';
 export default defineComponent({
+  components: {
+    UserInfo
+  },
   emit: ['foldChange'],
   setup(props, { emit }) {
     const isFold = ref(false);
@@ -26,10 +34,18 @@ export default defineComponent({
 });
 </script>
 <style lang="less" scoped>
-.fold-menu {
+.fold-menu-nav {
+  width: 100%;
+  display: flex;
   .fold-menu {
     font-size: 30px;
     cursor: pointer;
+  }
+  .content {
+    display: flex;
+    justify-content: space-between;
+    flex: 1;
+    padding: 0 20px;
   }
 }
 </style>
