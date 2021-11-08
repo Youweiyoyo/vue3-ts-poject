d4401800311f82cacb49d20fd4e0216d3c1c0d85
Youwei <yoyoyo9527@163.com>
2021-11-08 22:31:02 +0800

    升级vue版本

--- src/network/main/analysis/dashboard.ts
+++ src/network/main/analysis/dashboard.ts
@@ -4,29 +4,28 @@ enum DashboardAPI {
   categoryGoodsCount = '/goods/category/count',
   categoryGoodsSale = '/goods/category/sale',
   categoryGoodsFavor = '/goods/category/favor',
-  addressGoodsSale = '/goods/address/sale',
+  addressGoodsSale = '/goods/address/sale'
 }
 
 export function getCategoryGoodsCount() {
   return Service.get({
     url: DashboardAPI.categoryGoodsCount
-  })
+  });
 }
 
 export function getCategoryGoodsSale() {
   return Service.get({
     url: DashboardAPI.categoryGoodsSale
-  })
+  });
 }
 
 export function getCategoryGoodsFavor() {
   return Service.get({
     url: DashboardAPI.categoryGoodsFavor
-  })
+  });
 }
 export function getAddressGoodsSale() {
   return Service.get({
     url: DashboardAPI.addressGoodsSale
-  })
+  });
 }
-
